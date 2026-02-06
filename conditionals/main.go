25a475d638159f0b2283cf243758c1d67473a4fb
aprimr <aprimregmi24@gmail.com>
2026-02-06 21:38:59 +0545

    switch case in go

--- conditionals/main.go
+++ conditionals/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
 	// if else
@@ -27,4 +29,43 @@ func main(){
 	// } else if age < 18{
 	// 	fmt.Println("Kid" , age)
 	// }
+
+
+	// SWITCH STATEMENT //
+	// number:= 5
+	// switch number{
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// case 3: 
+	// 	fmt.Println("three")
+	// default:
+	// 	fmt.Println("other")
+	// }
+
+	// MULTIPLE CONDITIONS SWITCH //
+	// switch time.Now().Weekday(){
+	// 	case time.Sunday, time.Saturday: 
+	// 		fmt.Println("Weekend")
+	// default:
+	// 	fmt.Println("WorkDay")
+	// }
+
+	typeOf := func(n any){
+		switch n.(type){
+		case string:
+			fmt.Println("String")
+		case int:
+			fmt.Println("Int")
+		case bool:
+			fmt.Println("Boolean")
+		default:
+			fmt.Println("Other")
+		}
+	}
+
+	typeOf("golang")
+	typeOf(12)
+	typeOf(true)
 }
