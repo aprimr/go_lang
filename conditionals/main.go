db9e6eeb5f47904c6ebec8829f4085aa01be96ce
aprimr <aprimregmi24@gmail.com>
2026-02-06 21:24:17 +0545

    if else in go

---
+++ conditionals/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main(){
+	// if else
+	// age:=12
+	// if age>18{
+	// 	fmt.Println("You are adult")
+	// } else {
+	// 	fmt.Println("You are not adult")
+	// }
+
+	// if.... else if.... else
+	// salary := 300
+	// if salary>=200 {
+	// 	fmt.Println("Boss")
+	// } else if salary>100 && salary<200 {
+	// 	fmt.Println("Manager")
+	// } else {
+	// 	fmt.Println("Employee")
+	// }
+
+	// We can declare a variable inside if constructor
+	// if age:=20; age>18{
+	// 	fmt.Println("Adult", age)
+	// } else if age < 18{
+	// 	fmt.Println("Kid" , age)
+	// }
+}
