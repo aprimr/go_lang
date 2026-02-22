ba7c274ed8d5e935ae5ad17890a5203ba28900e5
aprimr <aprimregmi24@gmail.com>
2026-02-22 14:28:41 +0545

    wait groups

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func display(num int) {
+func display(num int, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println("Printing num", num)
 }
 
 func main() {
+	var wg sync.WaitGroup
+
 	for i := 0; i <= 10; i++ {
-		go display(i)
+		wg.Add(1)
+		go display(i, &wg)
 	}
 
-	time.Sleep(time.Second * 2)
+	wg.Wait()
 }
