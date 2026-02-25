a00e114eae6fabacbad88b67156f6bb3be8fab6c
aprimr <aprimregmi24@gmail.com>
2026-02-25 20:50:13 +0545

    calculator api in go lang

--- nethttp/main.go
+++ nethttp/main.go
@@ -77,49 +77,121 @@ import (
 // }
 
 // Store the decoded json into struct
-type User struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+// type User struct {
+// 	Name string `json:"name"`
+// 	Age  int    `json:"age"`
+// }
+
+// func echoHandler(w http.ResponseWriter, r *http.Request) {
+// 	defer r.Body.Close()
+// 	fmt.Println("request recieved")
+
+// 	if r.Method != http.MethodPost {
+// 		http.Error(w, "Cannot use this method", http.StatusMethodNotAllowed)
+// 		return
+// 	}
+
+// 	var user User
+
+// 	// decode JOSN recieved from request body
+// 	err := json.NewDecoder(r.Body).Decode(&user)
+// 	if err != nil {
+// 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	// set response header
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	// Build response
+// 	response := map[string]any{
+// 		"message": "recieved",
+// 		"data":    user,
+// 	}
+
+// 	// Send response
+// 	json.NewEncoder(w).Encode(response)
+// }
+
+// func main() {
+// 	port := ":8080"
+// 	mux := http.NewServeMux()
+
+// 	// POST method `/echo`
+// 	mux.HandleFunc("/echo", echoHandler)
+
+// 	fmt.Println("Server starting on port", port)
+// 	http.ListenAndServe(port, mux)
+// }
+
+// // calculator
+
+type Calculator struct {
+	Val1     float64 `json:"value1"`
+	Val2     float64 `json:"value2"`
+	Operator string  `json:"operator"`
 }
 
-func echoHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	fmt.Println("request recieved")
+type CalculateResult struct {
+	Result float64 `json:"result"`
+}
 
+func calculateHandler(w http.ResponseWriter, r *http.Request) {
+	// check if the request method is POST
 	if r.Method != http.MethodPost {
 		http.Error(w, "Cannot use this method", http.StatusMethodNotAllowed)
-		return
 	}
 
-	var user User
+	var calc Calculator
+	var result float64
 
-	// decode JOSN recieved from request body
-	err := json.NewDecoder(r.Body).Decode(&user)
+	// decode JSON
+	err := json.NewDecoder(r.Body).Decode(&calc)
 	if err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	// close request
+	r.Body.Close()
+
+	// switch the operator
+	switch calc.Operator {
+	case "add":
+		result = calc.Val1 + calc.Val2
+	case "sub":
+		result = calc.Val1 - calc.Val2
+	case "mul":
+		result = calc.Val1 * calc.Val2
+	case "div":
+		if calc.Val2 == 0 {
+			http.Error(w, "Cannot divide by zero", http.StatusBadRequest)
+			return
+		}
+		result = calc.Val1 / calc.Val2
+	default:
+		http.Error(w, "Invalid operation", http.StatusBadRequest)
 		return
 	}
 
 	// set response header
 	w.Header().Set("Content-Type", "application/json")
 
-	// Build response
-	response := map[string]any{
-		"message": "recieved",
-		"data":    user,
+	// build response body
+	resBody := map[string]any{
+		"result": result,
 	}
 
-	// Send response
-	json.NewEncoder(w).Encode(response)
+	// send response
+	json.NewEncoder(w).Encode(resBody)
 }
 
 func main() {
 	port := ":8080"
 	mux := http.NewServeMux()
 
-	// POST method `/echo`
-	mux.HandleFunc("/echo", echoHandler)
+	// route for calculation
+	mux.HandleFunc("/calc", calculateHandler)
 
-	fmt.Println("Server starting on port", port)
+	fmt.Println("Server started on port", port)
 	http.ListenAndServe(port, mux)
 }
